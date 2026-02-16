2e41234c1d0776009bfdd6fdf29b9e9c1eb8ca92
Vpee <you@email.com>
2026-02-16 14:58:46 +0200

    fixed

--- src/pages/workspace/layout.jsx
+++ src/pages/workspace/layout.jsx
@@ -90,7 +90,8 @@ useEffect(() => {
   const title = titleMap[location.pathname] || "Workspace";
 
   return (
-    <div className="flex w-full min-h-screen bg-[#12141A]">
+    <div className="flex w-full min-h-[100svh] bg-[#12141A]">
+
 
       {/* DESKTOP TOOL SHELL */}
       <aside className="hidden lg:block h-screen w-[80px] flex-shrink-0 z-50">
@@ -153,7 +154,8 @@ useEffect(() => {
       <div
         ref={scrollRef}
         id="workspace-scroll"
-        className="flex flex-col flex-1 h-screen overflow-y-auto"
+       className="flex flex-col flex-1 h-[100svh] overflow-y-auto overscroll-contain pb-[env(safe-area-inset-bottom)]"
+
       >
         <div
           className={`
